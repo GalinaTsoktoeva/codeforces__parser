84c52114a64e19f2d4478b68f74cd398c0b84ecf
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-10-14 14:08:13 +0300

    added test

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import os
+
+import django
+import pytest
+
+from task.services import Codeforces
+
+
+@pytest.fixture()
+def some_api_codeforces():
+    con = Codeforces()
+    return con
+
+
+# @pytest.fixture()
+# def some_task():
+#     new_task = Task.objects.create(
+#         name="Diamond Theft",
+#         tags=['bitmasks', 'dp', 'greedy', 'math', 'sortings', 'two pointers'],
+#         count_solutions=502,
+#         numbers="1886E",
+#         index="E",
+#         complexity="1000",
+#         number_contest='1886'
+#     )
+#     return new_task
---
+++ tests/test_codeforces.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+def test_codeforces(some_api_codeforces):
+    assert some_api_codeforces.name == "codeforces_conn"
+    assert some_api_codeforces.url == 'https://codeforces.com/api/'
+
+
+def test_get_tasks(some_api_codeforces):
+    result = some_api_codeforces.get_tasks(query="../data.json")
+    print(result)
+    assert result == 1
+
